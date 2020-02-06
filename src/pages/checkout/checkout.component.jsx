1ac34bc118a4c0ab07e63659ad58df0afe2547e6
Kreshnik Sadriu <sadrikre@students.zhaw.ch>
2020-02-06 18:20:05 +0100

    fix structure & refactoring app

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -2,9 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 
-
 import CheckoutItem from '../../components/checkout-item/checkout-item.component';
-
 
 import {
   selectCartItems,
@@ -33,7 +31,7 @@ const CheckoutPage = ({ cartItems, total }) => (
       </div>
     </div>
     {cartItems.map(cartItem => (
-        <CheckoutItem key ={cartItem.id} cartItem ={cartItem} />
+      <CheckoutItem key={cartItem.id} cartItem={cartItem} />
     ))}
     <div className='total'>TOTAL: ${total}</div>
   </div>
